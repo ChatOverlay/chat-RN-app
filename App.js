3004025b7391f0b1c4bcd7be763f9edf9cc74014
Xingu <ojyoung2490@gmail.com>
2024-05-26 18:22:23 +0900

    Dev : splash 화면 추가

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+import { WebView as MobileWebView } from 'react-native-webview';
+import WebWebView from 'react-native-web-webview';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const WebView = Platform.select({
+  web: WebWebView,
+  default: MobileWebView,
+});
 
 export default function App() {
+  const [isWebViewLoaded, setIsWebViewLoaded] = useState(false);
+
+  useEffect(() => {
+    if (isWebViewLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [isWebViewLoaded]);
+
   return (
-    <WebView
-      source={{ uri: 'https://www.clasome.com' }}
-    />
+    <View style={styles.container}>
+      <WebView
+        source={{ uri: 'https://www.clasome.com' }}
+        style={{ flex: 1}}
+        onLoadEnd={() => setIsWebViewLoaded(true)}
+      />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
