4bf8a12fee90ed964541412ee4915b02ae211949
Xingu <ojyoung2490@gmail.com>
2024-06-09 18:50:05 +0900

    design : 상단 바 색상 조정

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, Platform } from 'react-native';
 import { WebView as MobileWebView } from 'react-native-webview';
 import WebWebView from 'react-native-web-webview';
 import * as SplashScreen from 'expo-splash-screen';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -21,22 +23,30 @@ export default function App() {
   }, [isWebViewLoaded]);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ExpoStatusBar style="dark" translucent={true} backgroundColor="transparent" />
       <WebView
         source={{ uri: 'https://www.clasome.com' }}
         mixedContentMode="always"
         style={styles.webview}
         onLoadEnd={() => setIsWebViewLoaded(true)}
+        javaScriptEnabled={true}
+        injectedJavaScript={`
+          document.body.style.backgroundColor = 'transparent';
+          true;
+        `}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'transparent',
   },
   webview: {
     flex: 1,
+    backgroundColor: 'transparent',
   },
 });
